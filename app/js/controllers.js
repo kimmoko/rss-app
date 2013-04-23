108d8c9eb6f25fe27beb4d39e52ad74de71607cd
kimmoko <kimmo.kononen@gmail.com>
2013-04-23 21:40:51 +0300

    View1 shows rss feed as json

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,17 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
+	$scope.teksti = 'Foobar!';
+	$scope.rssFeed = 'http://showrss.karmorra.info/feeds/schedule/schedule.rss';
+	var url = 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=5&callback=JSON_CALLBACK&q=' + $scope.rssFeed;
+	
+	$http.jsonp(url)
+    .success(function(data){
+        console.log(data.responseData.feed);
+		$scope.data = data;
+    });
+	
   }])
   .controller('MyCtrl2', [function() {
 
